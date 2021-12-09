e8027a07ae936c2ca77bb4b70e5d0185f536944d
DianaShymkiv <shymkiv.diana@gmail.com>
2021-12-09 11:00:43 +0000

    additional 10

--- lesson10/additional10/add10.js
+++ lesson10/additional10/add10.js
@@ -52,191 +52,190 @@ let usersWithAddress = [
 // 3й - оставляет тех у кого город киев
 // Данные выводить в документ
 
-// let form = document.createElement("div");
-//
-// let checkStatus = document.createElement('input');
-// checkStatus.setAttribute('type', 'checkbox');
-//
-// let checkAge = document.createElement('input');
-// checkAge.setAttribute('type', 'checkbox');
-//
-// let checkCity = document.createElement('input');
-// checkCity.setAttribute('type', 'checkbox');
-//
-// let button = document.createElement('button');
-// button.innerText = 'check';
-//
-// document.body.appendChild(form);
-// form.append(checkStatus, checkAge, checkCity, button);
-//
-// button.onclick = function (e) {
-//     e.preventDefault();
-//     let status = checkStatus.checked;
-//     let age = checkAge.checked;
-//     let city = checkCity.checked;
-//     let arr = [];
-//
-//     if (status && age && city) {
-//         arr = usersWithAddress.filter(value => value.status && value.age >= 29 && value.address.city === 'Kyiv');
-//     } else if (status && age) {
-//         arr = usersWithAddress.filter(value => value.status && value.age >= 29);
-//     } else if (status && city) {
-//         arr = usersWithAddress.filter(value => value.status && value.address.city === 'Kyiv');
-//     } else if (age && city) {
-//         arr = usersWithAddress.filter(value => value.age >= 29 && value.address.city === 'Kyiv');
-//     } else if (status) {
-//         arr = usersWithAddress.filter(value => value.status);
-//     } else if (age) {
-//         arr = usersWithAddress.filter(value => value.age >= 29);
-//     } else if (city) {
-//         arr = usersWithAddress.filter(value => value.address.city === 'Kyiv');
-//     }
-//
-//     let div = document.createElement("div");
-//     document.body.appendChild(div);
-//
-//     let ul = document.createElement("ul");
-//     div.appendChild(ul);
-//
-//     for (const user of arr) {
-//         for (const key in user) {
-//             if (typeof user[key] === 'object') {
-//                 for (const key1 in user[key]) {
-//                     let li = document.createElement('li');
-//                     li.innerText = `${key1} ${user[key][key1]}`;
-//                     ul.appendChild(li);
-//                 }
-//             } else {
-//                 let li = document.createElement('li');
-//                 li.innerText = `${key} ${user[key]}`;
-//                 ul.appendChild(li);
-//
-//             }
-//         }
-//
-//     }
-//
-// }
+let form = document.createElement("div");
+
+let checkStatus = document.createElement('input');
+checkStatus.setAttribute('type', 'checkbox');
+
+let checkAge = document.createElement('input');
+checkAge.setAttribute('type', 'checkbox');
+
+let checkCity = document.createElement('input');
+checkCity.setAttribute('type', 'checkbox');
+
+let button = document.createElement('button');
+button.innerText = 'check';
+
+document.body.appendChild(form);
+form.append(checkStatus, checkAge, checkCity, button);
+
+button.onclick = function (e) {
+    let status = checkStatus.checked;
+    let age = checkAge.checked;
+    let city = checkCity.checked;
+    let arr = [];
+
+    if (status && age && city) {
+        arr = usersWithAddress.filter(value => value.status && value.age >= 29 && value.address.city === 'Kyiv');
+    } else if (status && age) {
+        arr = usersWithAddress.filter(value => value.status && value.age >= 29);
+    } else if (status && city) {
+        arr = usersWithAddress.filter(value => value.status && value.address.city === 'Kyiv');
+    } else if (age && city) {
+        arr = usersWithAddress.filter(value => value.age >= 29 && value.address.city === 'Kyiv');
+    } else if (status) {
+        arr = usersWithAddress.filter(value => value.status);
+    } else if (age) {
+        arr = usersWithAddress.filter(value => value.age >= 29);
+    } else if (city) {
+        arr = usersWithAddress.filter(value => value.address.city === 'Kyiv');
+    }
+
+    let div = document.createElement("div");
+    document.body.appendChild(div);
+
+    let ul = document.createElement("ul");
+    div.appendChild(ul);
+
+    for (const user of arr) {
+        for (const key in user) {
+            if (typeof user[key] === 'object') {
+                for (const key1 in user[key]) {
+                    let li = document.createElement('li');
+                    li.innerText = `${key1} ${user[key][key1]}`;
+                    ul.appendChild(li);
+                }
+            } else {
+                let li = document.createElement('li');
+                li.innerText = `${key} ${user[key]}`;
+                ul.appendChild(li);
+
+            }
+        }
+
+    }
+
+}
 
 // *****(Прям овердоз с рекурсией) Создать функцию которая принимает какой-либо элемент DOM-структуры .Функция создает в боди 2 кнопки (назад/вперед)
 // при нажатии вперед, вы переходите к дочернему элементу, при еще одном нажатии на "вперед", вы переходите к следующему дочернему элементу (лежащему на одном уровне)
 // НО если у (какого-либо)дочеренего элемента есть дети, то нажатие "вперед" позволяет нам войти внутрь элемента и  выводит первого ребенка. и тд.
 //     Когда все дети заканчиваются, мы выходим из данного дочернего элемента и переходим к следующему, лежащему с ним на одном уровне
 
-const wrap = document.getElementsByClassName('wrap')[0];
-
-const xxx = (startElement) => {
-    startElement.classList.add('current');
-    const current = document.createElement('div');
-    const prevButton = document.createElement('button');
-    prevButton.innerText = 'previous element';
-    const nextButton = document.createElement('button');
-    nextButton.innerText = 'next element';
-    startElement.parentNode.appendChild(current);
-    current.appendChild(startElement);
-    current.parentNode.append(prevButton, nextButton);
-
-    let nextCheck = true;
-
-    const steep = (list) => {
-        let element;
-
-        if (nextCheck) {
-            element = list[0];
-            while (element.firstElementChild) {
-                element = element.firstElementChild
-            }
-        } else {
-            element = list[list.length - 1];
-        }
-
-        element.style.backgroundColor = 'lightGrey';
-        nextButton.onclick = () => {
-            nextCheck = true;
-
-            if (element.nextElementSibling) {
-                element = element.nextElementSibling;
-                element.style.backgroundColor = 'lightGrey';
-                element.previousElementSibling.style.backgroundColor = null;
-
-                if (element.children.length) {
-                    element.style.backgroundColor = null;
-
-                    while (element.firstElementChild?.children.length) {
-                        element = element.firstElementChild;
-                    }
-
-                    steep(element.children);
-                }
-
-            } else {
-
-                element.style.backgroundColor = null;
-                while (!element.parentElement.nextElementSibling) {
-
-                    if (element.parentElement.className.includes('current')) {
-
-                        if (element.children.length) {
-                            element = element.lastElementChild;
-                        }
-
-                        element.style.backgroundColor = 'lightGrey';
-                        return;
-                    }
-
-                    element = element.parentElement;
-                }
-
-                element = element.parentElement.nextElementSibling;
-                element.style.backgroundColor = 'lightGrey';
-            }
-        }
-
-        prevButton.onclick = () => {
-
-            nextCheck = false;
-
-            if (element.previousElementSibling) {
-                element = element.previousElementSibling;
-                element.style.backgroundColor = 'lightGrey';
-                element.nextElementSibling.style.backgroundColor = null;
-
-                if (element.children.length) {
-                    element.style.backgroundColor = null;
-
-                    while (element.lastElementChild?.children.length) {
-                        element = element.lastElementChild;
-                    }
-
-                    steep(element.children);
-                }
-
-            } else {
-                element.style.backgroundColor = null;
-
-                while (!element.parentElement.previousElementSibling) {
-
-                    if (element.parentElement.className.includes('current')) {
-
-                        while (element.firstElementChild) {
-                            element = element.firstElementChild;
-                        }
-
-                        element.style.backgroundColor = 'lightGrey';
-                        return;
-                    }
-
-                    element = element.parentElement;
-                }
-
-                element = element.parentElement.previousElementSibling;
-                element.style.backgroundColor = 'lightGrey';
-            }
-        }
-    }
-    steep(startElement.children);
-}
-xxx(wrap);
+// const wrap = document.getElementsByClassName('wrap')[0];
+//
+// const xxx = (startElement) => {
+//     startElement.classList.add('current');
+//     const current = document.createElement('div');
+//     const prevButton = document.createElement('button');
+//     prevButton.innerText = 'previous element';
+//     const nextButton = document.createElement('button');
+//     nextButton.innerText = 'next element';
+//     startElement.parentNode.appendChild(current);
+//     current.appendChild(startElement);
+//     current.parentNode.append(prevButton, nextButton);
+//
+//     let nextCheck = true;
+//
+//     const steep = (list) => {
+//         let element;
+//
+//         if (nextCheck) {
+//             element = list[0];
+//             while (element.firstElementChild) {
+//                 element = element.firstElementChild
+//             }
+//         } else {
+//             element = list[list.length - 1];
+//         }
+//
+//         element.style.backgroundColor = 'lightGrey';
+//         nextButton.onclick = () => {
+//             nextCheck = true;
+//
+//             if (element.nextElementSibling) {
+//                 element = element.nextElementSibling;
+//                 element.style.backgroundColor = 'lightGrey';
+//                 element.previousElementSibling.style.backgroundColor = null;
+//
+//                 if (element.children.length) {
+//                     element.style.backgroundColor = null;
+//
+//                     while (element.firstElementChild?.children.length) {
+//                         element = element.firstElementChild;
+//                     }
+//
+//                     steep(element.children);
+//                 }
+//
+//             } else {
+//
+//                 element.style.backgroundColor = null;
+//                 while (!element.parentElement.nextElementSibling) {
+//
+//                     if (element.parentElement.className.includes('current')) {
+//
+//                         if (element.children.length) {
+//                             element = element.lastElementChild;
+//                         }
+//
+//                         element.style.backgroundColor = 'lightGrey';
+//                         return;
+//                     }
+//
+//                     element = element.parentElement;
+//                 }
+//
+//                 element = element.parentElement.nextElementSibling;
+//                 element.style.backgroundColor = 'lightGrey';
+//             }
+//         }
+//
+//         prevButton.onclick = () => {
+//
+//             nextCheck = false;
+//
+//             if (element.previousElementSibling) {
+//                 element = element.previousElementSibling;
+//                 element.style.backgroundColor = 'lightGrey';
+//                 element.nextElementSibling.style.backgroundColor = null;
+//
+//                 if (element.children.length) {
+//                     element.style.backgroundColor = null;
+//
+//                     while (element.lastElementChild?.children.length) {
+//                         element = element.lastElementChild;
+//                     }
+//
+//                     steep(element.children);
+//                 }
+//
+//             } else {
+//                 element.style.backgroundColor = null;
+//
+//                 while (!element.parentElement.previousElementSibling) {
+//
+//                     if (element.parentElement.className.includes('current')) {
+//
+//                         while (element.firstElementChild) {
+//                             element = element.firstElementChild;
+//                         }
+//
+//                         element.style.backgroundColor = 'lightGrey';
+//                         return;
+//                     }
+//
+//                     element = element.parentElement;
+//                 }
+//
+//                 element = element.parentElement.previousElementSibling;
+//                 element.style.backgroundColor = 'lightGrey';
+//             }
+//         }
+//     }
+//     steep(startElement.children);
+// }
+// xxx(wrap);
 
 // - Напишите «Карусель» – ленту изображений, которую можно листать влево-вправо нажатием на стрелочки.
 
